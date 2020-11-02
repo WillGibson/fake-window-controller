1b67e70c63b75af0d023865d7689914d0ee705a4
Will Gibson <will.gibson@madetech.com>
2020-11-02 16:31:41 +0000

    Make subjective brightness correspond with linear scale (enough)

--- fake-window-controller.ino
+++ fake-window-controller.ino
@@ -1,30 +1,49 @@
-
 // CONFIGURATION
 #define WINDOW_BRIGHTNESS_PIN 3
-#define MAX_BRIGHTNESS 20 // 0-255
-#define MIN_BRIGHTNESS 1 // 0-255
-#define FADE_INTERVAL 75 // ms between fade steps
+#define MIN_BRIGHTNESS 0 // 0 - 100
+#define MAX_BRIGHTNESS 100 // 0 - 100
+#define BRIGHTNESS_INCREMENT_SIZE 2
+#define FADE_STEP_INTERVAL 50
+#define ZERO_OFFSET 15 // No light comes out below this so don't use it
+#define DEBUG 0 // 0 or 1
 
 // DECLARATIONS
-byte desiredLightLevel;
-byte currentLightLevel;
+float desiredLightLevel;
+float adjustedMinBrightness;
+float currentLightLevel;
+float currentLightLevelForPin;
+float cubeRootOf255 = 6.34;
 unsigned long timeForNextFadeStep;
 
 void setup() {
-  Serial.begin(9600);
+  if (DEBUG == 1) Serial.begin(9600);
+  adjustedMinBrightness = MIN_BRIGHTNESS + ZERO_OFFSET;
   desiredLightLevel = MAX_BRIGHTNESS;
-  Serial.println((String)"Initial desiredLightLevel: " + desiredLightLevel);
+  currentLightLevel = adjustedMinBrightness;
 }
 
 void loop() {
   if (millis() > timeForNextFadeStep) {
-    timeForNextFadeStep = millis() + (unsigned long)FADE_INTERVAL;
-    if (currentLightLevel == MAX_BRIGHTNESS) desiredLightLevel = MIN_BRIGHTNESS;
-    if (currentLightLevel == MIN_BRIGHTNESS) desiredLightLevel = MAX_BRIGHTNESS;
-    if (currentLightLevel < desiredLightLevel) currentLightLevel++;
-    if (currentLightLevel > desiredLightLevel) currentLightLevel--;
-    Serial.println((String)"desiredLightLevel: " + desiredLightLevel);
-    Serial.println((String)"currentLightLevel:" + currentLightLevel);
-    analogWrite(WINDOW_BRIGHTNESS_PIN, currentLightLevel);
+    timeForNextFadeStep = millis() + (unsigned long)FADE_STEP_INTERVAL;
+
+    if (currentLightLevel < desiredLightLevel) currentLightLevel += BRIGHTNESS_INCREMENT_SIZE;
+    else if (currentLightLevel > desiredLightLevel) currentLightLevel -= BRIGHTNESS_INCREMENT_SIZE;
+
+    currentLightLevelForPin = min(
+                                255,
+                                pow((float)(currentLightLevel / 100) * cubeRootOf255, 3)
+                              );
+    log(
+      (String)"currentLightLevel // currentLightLevelForPin: " +
+      currentLightLevel + " // " + currentLightLevelForPin
+    );
+
+    if (currentLightLevel >= MAX_BRIGHTNESS) desiredLightLevel = adjustedMinBrightness;
+    if (currentLightLevel <= adjustedMinBrightness) desiredLightLevel = MAX_BRIGHTNESS;
+    analogWrite(WINDOW_BRIGHTNESS_PIN, currentLightLevelForPin);
   }
+}
+
+void log(String message) {
+  if (DEBUG == 1) Serial.println(message);
 }
